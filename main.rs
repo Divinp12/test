cb3a0f8d46e9d6826e0f21253b46f8d8a5d9f2ea
Divinp123 <142677856+Divinp12@users.noreply.github.com>
2025-08-22 23:33:24 +0000

    Update main.rs

--- main.rs
+++ main.rs
@@ -2,16 +2,14 @@ use std::process::{Command, Stdio};
 
 fn main() {
     let status = Command::new("pacman")
-        .arg("-Syu")
-        .arg("--noconfirm")
-        .arg("--quiet")
+        .arg("-Sy")
         .stdout(Stdio::null())
         .stderr(Stdio::null())
         .status()
         .expect("Falha ao executar o comando");
 
     if status.success() {
-        println!("Atualização concluída com sucesso!");
+        println!("Atualização concluída");
     } else {
         eprintln!("Falha na atualização.");
     }
