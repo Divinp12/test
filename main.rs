9725041345d82752874352f91e1ff66e07496da2
Divinp123 <142677856+Divinp12@users.noreply.github.com>
2025-08-23 20:20:44 -0300

    Update main.rs

--- main.rs
+++ main.rs
@@ -2,35 +2,34 @@ use std::process::{Command, Stdio};
 
 fn main() {
 
-        let status1 = Command::new("echo")
-            .arg("'Server=https://mirror.ufscar.br/archlinux/\$repo/os/\$arch'")
-            .arg(">")
-            .arg("/etc/pacman.d/mirrorlist")
+        println!("adicionando espelho brasileiro");
+
+        let status1 = Command::new("sh")
+            .arg("-c")
+            .arg("echo 'Server=https://mirror.ufscar.br/archlinux/\\$repo/os/\\$arch' > /etc/pacman.d/mirrorlist")
             .stdout(Stdio::null())
             .stderr(Stdio::null())
             .status()
-            .expect("Falha ao executar o comando");
+            .expect("algo deu errado ao executar comando de adicionar espelho brasileiro");
 
                if status1.success() {
-                  println!("atualização concluída");
+                  println!("sucesso");
                      let _ = Command::new("clear").status();}
 
+        
+        println!("adicionando espelho brasileiro");
+        
+        let status1 = Command::new("sh")
+            .arg("-c")
+            .arg("echo 'Server=https://mirror.ufscar.br/archlinux/\\$repo/os/\\$arch' > /etc/pacman.d/mirrorlist")
+            .stdout(Stdio::null())
+            .stderr(Stdio::null())
+            .status()
+            .expect("algo deu errado ao executar comando de adicionar espelho brasileiro");
 
-
-    
-    println!("atualizando repositórios");
-    
-        let status3 = Command::new("pacman")
-        .arg("-Sy")
-        .stdout(Stdio::null())
-        .stderr(Stdio::null())
-        .status()
-        .expect("Falha ao executar o comando");
-
-    if status3.success() {
-        println!("atualização concluída");
-        let _ = Command::new("clear").status();
-}
+               if status1.success() {
+                  println!("sucesso");
+                     let _ = Command::new("clear").status();}
         
         else {
         eprintln!("falha na atualização");
