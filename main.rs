86c4557d7081e09a36fd9fe2d22f920b9abdd353
Divinp123 <142677856+Divinp12@users.noreply.github.com>
2025-08-23 16:55:40 +0000

    Update main.rs

--- main.rs
+++ main.rs
@@ -1,18 +1,38 @@
 use std::process::{Command, Stdio};
 
 fn main() {
-    println!("Atualizando repositórios");
+
+        let status1 = Command::new("echo")
+            .arg("'Server=https://mirror.ufscar.br/archlinux/\$repo/os/\$arch'")
+            .arg(">")
+            .arg("/etc/pacman.d/mirrorlist")
+            .stdout(Stdio::null())
+            .stderr(Stdio::null())
+            .status()
+            .expect("Falha ao executar o comando");
+
+               if status1.success() {
+                  println!("atualização concluída");
+                     let _ = Command::new("clear").status();}
+
+
+
     
-    let status = Command::new("pacman")
+    println!("atualizando repositórios");
+    
+        let status3 = Command::new("pacman")
         .arg("-Sy")
         .stdout(Stdio::null())
         .stderr(Stdio::null())
         .status()
         .expect("Falha ao executar o comando");
 
-    if status.success() {
-        println!("Atualização concluída");
-    } else {
-        eprintln!("Falha na atualização");
+    if status3.success() {
+        println!("atualização concluída");
+        let _ = Command::new("clear").status();
+}
+        
+        else {
+        eprintln!("falha na atualização");
     }
 }
