ee5269c1982b0eab900f0ee929a2b91bd1cbc8db
Divinp123 <142677856+Divinp12@users.noreply.github.com>
2025-08-23 20:30:04 -0300

    Update main.rs

--- main.rs
+++ main.rs
@@ -16,20 +16,33 @@ fn main() {
                   println!("sucesso");
                      let _ = Command::new("clear").status();}
 
+
         
-        println!("adicionando espelho brasileiro");
+        println!("sobscrevendo arquivo pacman.conf");
         
-        let status1 = Command::new("sh")
+        let status2 = Command::new("sh")
             .arg("-c")
-            .arg("echo 'Server=https://mirror.ufscar.br/archlinux/\\$repo/os/\\$arch' > /etc/pacman.d/mirrorlist")
+            .arg("echo '[options]
+Architecture=auto
+CheckSpace
+ParallelDownloads=1
+SigLevel=Required DatabaseOptional
+LocalFileSigLevel=Optional
+[core]
+Include=/etc/pacman.d/mirrorlist
+[extra]
+Include=/etc/pacman.d/mirrorlist
+[multilib]
+Include=/etc/pacman.d/mirrorlist' > /etc/pacman.conf")
             .stdout(Stdio::null())
             .stderr(Stdio::null())
             .status()
             .expect("algo deu errado ao executar comando de adicionar espelho brasileiro");
 
-               if status1.success() {
+               if status2.success() {
                   println!("sucesso");
-                     let _ = Command::new("clear").status();}
+                     let _ = Command::new("clear").status();
+               }
         
         else {
         eprintln!("falha na atualização");
