076e110e7b1644605a887f2448b1611a5d77acfc
Divinp123 <142677856+Divinp12@users.noreply.github.com>
2025-08-23 20:38:39 -0300

    Update main.rs

--- main.rs
+++ main.rs
@@ -2,27 +2,25 @@ use std::process::{Command, Stdio};
 
 fn main() {
 
-        println!("adicionando espelho brasileiro");
+println!("adicionando espelho brasileiro");
+let status1 = Command::new("sh")
+.arg("-c")
+.arg("echo 'Server=https://mirror.ufscar.br/archlinux/\\$repo/os/\\$arch' > /etc/pacman.d/mirrorlist")
+.stdout(Stdio::null())
+.stderr(Stdio::null())
+.status()
+.expect("algo deu errado ao executar comando de adicionar espelho brasileiro");
+if status1.success() {
+println!("sucesso");
+let _ = Command::new("clear").status();
+}
 
-        let status1 = Command::new("sh")
-            .arg("-c")
-            .arg("echo 'Server=https://mirror.ufscar.br/archlinux/\\$repo/os/\\$arch' > /etc/pacman.d/mirrorlist")
-            .stdout(Stdio::null())
-            .stderr(Stdio::null())
-            .status()
-            .expect("algo deu errado ao executar comando de adicionar espelho brasileiro");
-
-               if status1.success() {
-                  println!("sucesso");
-                     let _ = Command::new("clear").status();}
 
 
-        
-        println!("sobscrevendo arquivo pacman.conf");
-        
-        let status2 = Command::new("sh")
-            .arg("-c")
-            .arg("echo '[options]
+println!("sobscrevendo arquivo pacman.conf");
+let status2 = Command::new("sh")
+.arg("-c")
+.arg("echo '[options]
 Architecture=auto
 CheckSpace
 ParallelDownloads=1
@@ -34,15 +32,14 @@ Include=/etc/pacman.d/mirrorlist
 Include=/etc/pacman.d/mirrorlist
 [multilib]
 Include=/etc/pacman.d/mirrorlist' > /etc/pacman.conf")
-            .stdout(Stdio::null())
-            .stderr(Stdio::null())
-            .status()
-            .expect("algo deu errado ao executar comando de adicionar espelho brasileiro");
-
-               if status2.success() {
-                  println!("sucesso");
-                     let _ = Command::new("clear").status();
-               }
+.stdout(Stdio::null())
+.stderr(Stdio::null())
+.status()
+.expect("algo deu errado ao executar comando de sobscrever arquivo pacman.conf");
+if status2.success() {
+println!("sucesso");
+let _ = Command::new("clear").status();
+}
         
         else {
         eprintln!("falha na atualização");
