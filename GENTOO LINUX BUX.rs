1f0215cec0c16cf4d63996d3ed1572c7525413a7
Divinp123 <142677856+Divinp12@users.noreply.github.com>
2025-09-11 09:39:51 -0300

    Update GENTOO LINUX BUX.rs

--- GENTOO LINUX BUX.rs
+++ GENTOO LINUX BUX.rs
@@ -17,14 +17,14 @@ fn main() {
 
 
 println!("formatando 1 disco rigido valido");
-let status4 = Command::new("sh")
+let status1 = Command::new("sh")
 .arg("-c")
 .arg(r#"if dd if=/dev/zero of=/dev/nvme0n1p bs=64M && \
-parted /dev/nvme0n1p mklabel gpt && \
-parted /dev/nvme0n1p mkpart ESP fat32 1MiB 1025MiB && \
-parted /dev/nvme0n1p set 1 esp on && \
-parted /dev/nvme0n1p mkpart primary ext4 1025MiB 30721MiB && \
-parted /dev/nvme0n1p mkpart primary ext4 30721MiB 100% && \
+parted -s /dev/nvme0n1p mklabel gpt && \
+parted -s /dev/nvme0n1p mkpart ESP fat32 1MiB 1025MiB && \
+parted -s /dev/nvme0n1p set 1 esp on && \
+parted -s /dev/nvme0n1p mkpart primary ext4 1025MiB 30721MiB && \
+parted -s /dev/nvme0n1p mkpart primary ext4 30721MiB 100% && \
 mkfs.fat -F32 /dev/nvme0n1p1 && \
 mkfs.ext4 -F /dev/nvme0n1p2 && \
 mkfs.ext4 -F /dev/nvme0n1p3 && \
@@ -36,11 +36,11 @@ mount /dev/nvme0n1p1 /mnt/boot/EFI && \
 mount /dev/nvme0n1p3 /mnt/home
 else
 dd if=/dev/zero of=/dev/sda bs=64M && \
-parted /dev/sda mklabel gpt && \
-parted /dev/sda mkpart ESP fat32 1MiB 1025MiB && \
-parted /dev/sda set 1 esp on && \
-parted /dev/sda mkpart primary ext4 1025MiB 30721MiB && \
-parted /dev/sda mkpart primary ext4 30721MiB 100% && \
+parted -s /dev/sda mklabel gpt && \
+parted -s /dev/sda mkpart ESP fat32 1MiB 1025MiB && \
+parted -s /dev/sda set 1 esp on && \
+parted -s /dev/sda mkpart primary ext4 1025MiB 30721MiB && \
+parted -s /dev/sda mkpart primary ext4 30721MiB 100% && \
 mkfs.fat -F32 /dev/sda1 && \
 mkfs.ext4 -F /dev/sda2 && \
 mkfs.ext4 -F /dev/sda3 && \
@@ -55,7 +55,7 @@ fi"#)
 .stderr(Stdio::null())
 .stdin(Stdio::null())
 .status();
-match status4 {
+match status1 {
 Ok(status) if status.success() => {println!("sucesso");thread::sleep(Duration::from_secs(3));let _ = Command::new("clear").status();}
 Ok(_) => {println!("falha ao formata 1 disco rigido valido");}
 Err(e) => {eprintln!("erro ao executar o comando: {}", e);}
