1fd6907147a30c2494c04b032f6141f82dff81cf
Divinp123 <142677856+Divinp12@users.noreply.github.com>
2025-09-11 09:18:25 -0300

    Update GENTOO LINUX BUX.rs

--- GENTOO LINUX BUX.rs
+++ GENTOO LINUX BUX.rs
@@ -1,1 +1,47 @@
 
+println!("formatando 1 disco rigido valido");
+let status4 = Command::new("sh")
+.arg("-c")
+.arg(r#"if dd if=/dev/zero of=/dev/nvme0n1p bs=64M && \
+parted /dev/nvme0n1p mklabel gpt && \
+parted /dev/nvme0n1p mkpart ESP fat32 1MiB 1025MiB && \
+parted /dev/nvme0n1p set 1 esp on && \
+parted /dev/nvme0n1p mkpart primary ext4 1025MiB 30721MiB && \
+parted /dev/nvme0n1p mkpart primary ext4 30721MiB 100% && \
+partprobe && \
+mkfs.fat -F32 /dev/nvme0n1p1 && \
+mkfs.ext4 -F /dev/nvme0n1p2 && \
+mkfs.ext4 -F /dev/nvme0n1p3 && \
+mount /dev/nvme0n1p2 /mnt && \
+mkdir /mnt/boot && \
+mkdir /mnt/boot/EFI && \
+mkdir /mnt/home && \
+mount /dev/nvme0n1p1 /mnt/boot/EFI && \
+mount /dev/nvme0n1p3 /mnt/home
+else
+dd if=/dev/zero of=/dev/sda bs=64M && \
+parted /dev/sda mklabel gpt && \
+parted /dev/sda mkpart ESP fat32 1MiB 1025MiB && \
+parted /dev/sda set 1 esp on && \
+parted /dev/sda mkpart primary ext4 1025MiB 30721MiB && \
+parted /dev/sda mkpart primary ext4 30721MiB 100% && \
+partprobe && \
+mkfs.fat -F32 /dev/sda1 && \
+mkfs.ext4 -F /dev/sda2 && \
+mkfs.ext4 -F /dev/sda3 && \
+mount /dev/sda2 /mnt && \
+mkdir /mnt/boot && \
+mkdir /mnt/boot/EFI && \
+mkdir /mnt/home && \
+mount /dev/sda1 /mnt/boot/EFI && \
+mount /dev/sda3 /mnt/home
+fi"#)
+.stdout(Stdio::null())
+.stderr(Stdio::null())
+.stdin(Stdio::null())
+.status();
+match status4 {
+Ok(status) if status.success() => {println!("sucesso");thread::sleep(Duration::from_secs(3));let _ = Command::new("clear").status();}
+Ok(_) => {println!("falha ao formata 1 disco rigido valido");}
+Err(e) => {eprintln!("erro ao executar o comando: {}", e);}
+}
