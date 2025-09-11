8c36e6c46af83b4d668430cb20a3a737ca6fb0d1
Divinp123 <142677856+Divinp12@users.noreply.github.com>
2025-09-11 09:22:49 -0300

    Update GENTOO LINUX BUX.rs

--- GENTOO LINUX BUX.rs
+++ GENTOO LINUX BUX.rs
@@ -1,3 +1,20 @@
+use std::process::{Command, Stdio};
+use std::thread;
+use std::time::Duration;
+
+// thread::sleep(Duration::from_secs(3)); // pausa em 3 segundos. Para muda a duracao da pausa altere o numero depois de from_secs(
+// let _ = Command::new("clear")
+// .status();
+
+// .stdout(Stdio::null()) redirecionar mensagens para lugar nenhum.
+// .stderr(Stdio::null()) redirecionar mensagens de erro para lugar nenhum.
+// .stdin(Stdio::null())  redirecionar input de teclado, mouse, etc para lugar nenhum.
+
+
+// executa formatacao
+  
+fn main() {
+
 
 println!("formatando 1 disco rigido valido");
 let status4 = Command::new("sh")
@@ -8,7 +25,6 @@ parted /dev/nvme0n1p mkpart ESP fat32 1MiB 1025MiB && \
 parted /dev/nvme0n1p set 1 esp on && \
 parted /dev/nvme0n1p mkpart primary ext4 1025MiB 30721MiB && \
 parted /dev/nvme0n1p mkpart primary ext4 30721MiB 100% && \
-partprobe && \
 mkfs.fat -F32 /dev/nvme0n1p1 && \
 mkfs.ext4 -F /dev/nvme0n1p2 && \
 mkfs.ext4 -F /dev/nvme0n1p3 && \
@@ -25,7 +41,6 @@ parted /dev/sda mkpart ESP fat32 1MiB 1025MiB && \
 parted /dev/sda set 1 esp on && \
 parted /dev/sda mkpart primary ext4 1025MiB 30721MiB && \
 parted /dev/sda mkpart primary ext4 30721MiB 100% && \
-partprobe && \
 mkfs.fat -F32 /dev/sda1 && \
 mkfs.ext4 -F /dev/sda2 && \
 mkfs.ext4 -F /dev/sda3 && \
