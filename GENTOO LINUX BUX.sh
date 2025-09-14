8d5d5314b62a2f627bfa994aac5d8d3e3a70449d
Divinp123 <142677856+Divinp12@users.noreply.github.com>
2025-09-14 09:24:57 -0300

    Update GENTOO LINUX BUX.sh

--- GENTOO LINUX BUX.sh
+++ GENTOO LINUX BUX.sh
@@ -1,1 +1,16 @@
+#!/bin/bash
 
+dd if=/dev/zero of=/dev/sda bs=64M && \
+parted -s /dev/sda mklabel gpt && \
+parted -s /dev/sda mkpart ESP fat32 1MiB 900MiB && \
+parted -s /dev/sda set 1 esp on && \
+parted -s /dev/sda mkpart primary ext4 900MiB 30000MiB && \
+parted -s /dev/sda mkpart primary ext4 30000MiB 100% && \
+mkfs.fat -F32 /dev/sda1 && \
+mkfs.ext4 -F /dev/sda2 && \
+mkfs.ext4 -F /dev/sda3 && \
+mount /dev/sda2 /mnt/gentoo && \
+mkdir /mnt/gentoo/boot && \
+mount /dev/sda1 /mnt/gentoo/boot && \
+mkdir /mnt/gentoo/home && \
+mount /dev/sda3 /mnt/gentoo/home
