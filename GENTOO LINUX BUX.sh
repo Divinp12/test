1a023a708f7c1886482232d6e459b30cb7af327c
Divinp123 <142677856+Divinp12@users.noreply.github.com>
2025-09-14 09:53:57 -0300

    Update GENTOO LINUX BUX.sh

--- GENTOO LINUX BUX.sh
+++ GENTOO LINUX BUX.sh
@@ -16,7 +16,8 @@ mkdir /mnt/gentoo/home && \
 mount /dev/sda3 /mnt/gentoo/home && \
 wget -P /mnt/gentoo https://distfiles.gentoo.org/releases/amd64/autobuilds/20250907T165007Z/stage3-amd64-openrc-20250907T165007Z.tar.xz && \
 tar xvpf /mnt/gentoo/stage3-amd64-openrc-20250907T165007Z.tar.xz -C /mnt/gentoo --xattrs --numeric-owner && \
-echo 'MAKEOPTS"-j2"
+echo 'USE="bindist"
+MAKEOPTS"-j2"
 GENTOO_MIRRORS="http://gentoo.c3sl.ufpr.br/"' >> /mnt/gentoo/etc/portage/make.conf && \
 cp -L /etc/resolv.conf /mnt/gentoo/etc/ && \
 mount -t proc /proc /mnt/gentoo/proc/ && \
@@ -24,4 +25,16 @@ mount --rbind /sys /mnt/gentoo/sys && \
 mount --rbind /dev /mnt/gentoo/dev && \
 chroot /mnt/gentoo /bin/bash -c "source /etc/profile && \
 emerge-webrsync && \
-emerge --sync"
+emerge --sync && \
+eselect profile set 7 && \
+cp /usr/share/zoneinfo/America/Sao_Paulo /etc/localtime && \
+echo 'Sao_Paulo' > /etc/timezone && \
+echo 'pt_BR.UTF-8 UTF-8' > /etc/locale.gen && \
+locale-gen && \
+env-update && \
+source /etc/profile && \
+emerge sys-kernel/gentoo-sources && \
+echo '/dev/sda1 /boot vfat defaults 0 1' > /etc/fstab && \
+make clean -j$(nproc) -C /usr/src/linux*/ && \
+make mrproper -j$(nproc) -C /usr/src/linux*/ && \
+"
