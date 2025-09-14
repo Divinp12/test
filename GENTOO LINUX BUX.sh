99fd3ce0805af512248d96150876ed01cffd1403
Divinp123 <142677856+Divinp12@users.noreply.github.com>
2025-09-14 19:15:00 -0300

    Update GENTOO LINUX BUX.sh

--- GENTOO LINUX BUX.sh
+++ GENTOO LINUX BUX.sh
@@ -17,7 +17,7 @@ mount /dev/sda3 /mnt/gentoo/home && \
 wget -P /mnt/gentoo https://distfiles.gentoo.org/releases/amd64/autobuilds/20250907T165007Z/stage3-amd64-openrc-20250907T165007Z.tar.xz && \
 tar xvpf /mnt/gentoo/stage3-amd64-openrc-20250907T165007Z.tar.xz -C /mnt/gentoo --xattrs --numeric-owner && \
 echo 'USE="bindist -gnome"
-MAKEOPTS"-j2"
+MAKEOPTS="-j2"
 GENTOO_MIRRORS="http://gentoo.c3sl.ufpr.br/"' >> /mnt/gentoo/etc/portage/make.conf && \
 cp -L /etc/resolv.conf /mnt/gentoo/etc/ && \
 mount -t proc /proc /mnt/gentoo/proc/ && \
