4424cf89fabf121523d933aeef5af5a8f71402b4
Divinp123 <142677856+Divinp12@users.noreply.github.com>
2025-09-18 12:05:14 -0300

    Update GENTOO LINUX BUX.sh

--- GENTOO LINUX BUX.sh
+++ GENTOO LINUX BUX.sh
@@ -11,7 +11,8 @@ mkfs.ext4 -F /dev/sda2 && \
 mkfs.ext4 -F /dev/sda3 && \
 mount /dev/sda2 /mnt/gentoo && \
 mkdir /mnt/gentoo/boot && \
-mount /dev/sda1 /mnt/gentoo/boot && \
+mkdir /mnt/gentoo/boot/EFI && \
+mount /dev/sda1 /mnt/gentoo/boot/EFI && \
 mkdir /mnt/gentoo/home && \
 mount /dev/sda3 /mnt/gentoo/home && \
 wget -P /mnt/gentoo https://distfiles.gentoo.org/releases/amd64/autobuilds/20250907T165007Z/stage3-amd64-openrc-20250907T165007Z.tar.xz && \
