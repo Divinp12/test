5dee2469e612c05cce1092a7857a5c985e69ada2
Divinp123 <142677856+Divinp12@users.noreply.github.com>
2025-11-09 02:04:54 +0000

    Update GENTOO LINUX BUX.sh

--- GENTOO LINUX BUX.sh
+++ GENTOO LINUX BUX.sh
@@ -347,11 +347,11 @@ fi;
 
 
 echo "sobscrevendo arquivo make.conf";
-if echo \'COMMON_FLAGS="-O2 -g0 -s -DNDEBUG -fno-var-tracking -fno-record-gcc-switches -fno-ident -fno-mudflap"
-CFLAGS="-O2 -g0 -s -DNDEBUG -fno-var-tracking -fno-record-gcc-switches -fno-ident -fno-mudflap"
-CXXFLAGS="-O2 -g0 -s -DNDEBUG -fno-var-tracking -fno-record-gcc-switches -fno-ident -fno-mudflap"
-FCFLAGS="-O2 -g0 -s -DNDEBUG -fno-var-tracking -fno-record-gcc-switches -fno-ident -fno-mudflap"
-FFLAGS="-O2 -g0 -s -DNDEBUG -fno-var-tracking -fno-record-gcc-switches -fno-ident -fno-mudflap"
+if echo \'COMMON_FLAGS="-O2 -march=native -g0 -s -DNDEBUG -fno-var-tracking -fno-record-gcc-switches -fno-ident -fno-mudflap"
+CFLAGS="-O2 -march=native -g0 -s -DNDEBUG -fno-var-tracking -fno-record-gcc-switches -fno-ident -fno-mudflap"
+CXXFLAGS="-O2 -march=native -g0 -s -DNDEBUG -fno-var-tracking -fno-record-gcc-switches -fno-ident -fno-mudflap"
+FCFLAGS="-O2 -march=native -g0 -s -DNDEBUG -fno-var-tracking -fno-record-gcc-switches -fno-ident -fno-mudflap"
+FFLAGS="-O2 -march=native -g0 -s -DNDEBUG -fno-var-tracking -fno-record-gcc-switches -fno-ident -fno-mudflap"
 RUSTFLAGS="${RUSTFLAGS} -C target-cpu=native"
 MAKEOPTS="-j64"
 EMERGE_DEFAULT_OPTS="--keep-going=y --autounmask-write=y"
