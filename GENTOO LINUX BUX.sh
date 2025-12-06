5c14e2d9ac7d384b639a86a27f0e565ad5b64233
Divinp123 <142677856+Divinp12@users.noreply.github.com>
2025-12-06 19:03:36 -0300

    Update GENTOO LINUX BUX.sh

--- GENTOO LINUX BUX.sh
+++ GENTOO LINUX BUX.sh
@@ -19,7 +19,7 @@ mkdir /mnt/gentoo/boot/EFI && \
 mount /dev/sda1 /mnt/gentoo/boot/EFI && \
 mkdir /mnt/gentoo/home && \
 mount /dev/sda3 /mnt/gentoo/home && \
-wget -P /mnt/gentoo https://distfiles.gentoo.org/releases/amd64/autobuilds/20251109T170053Z/stage3-amd64-openrc-20251109T170053Z.tar.xz > /dev/null 2>&1 && \
+wget -P /mnt/gentoo https://distfiles.gentoo.org/releases/amd64/autobuilds/20251130T164554Z/stage3-amd64-openrc-20251130T164554Z.tar.xz > /dev/null 2>&1 && \
 tar xvpf /mnt/gentoo/stage3-*.tar.xz -C /mnt/gentoo --xattrs-include='*.*' --numeric-owner > /dev/null 2>&1 && \
 rm -rf /mnt/gentoo/stage3-*.tar.xz > /dev/null 2>&1 && \
 mkdir -p /mnt/gentoo/etc && \
