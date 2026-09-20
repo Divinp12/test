c1f5eb174eda6cdaa9272640f9ef3677b0b1cfc3
Divinp123 <142677856+Divinp12@users.noreply.github.com>
2026-09-20 00:58:49 +0000

    Update TINY.sh

--- TINY.sh
+++ TINY.sh
@@ -2,8 +2,8 @@
 clear;
 sudo pacman -Sy --noconfirm bc coreutils cpio gettext initramfs kmod libelf ncurses pahole perl python3 tar xz && \
 wget -P /tmp https://cdn.kernel.org/pub/linux/kernel/v7.x/linux-7.2.6.tar.xz && \
-tar
-sudo rm -rf /tmp/linux-7.2.6.tar.xz && \
+tar xvpf /tmp/linux-*.tar.xz -C /tmp --xattrs-include='*.*' --numeric-owner && \
+sudo rm -rf /tmp/linux-*.tar.xz && \
 make tinyconfig && \
 make -j$(nproc);
 
