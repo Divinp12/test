96683ee06d99ba693ca081b3a2a3215bb285c834
Divinp123 <142677856+Divinp12@users.noreply.github.com>
2026-09-20 01:59:44 +0000

    Update TINY.sh

--- TINY.sh
+++ TINY.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 clear;
-sudo pacman -Sy --noconfirm bc coreutils cpio gettext initramfs kmod libelf ncurses pahole perl python3 tar xz && \
+sudo pacman -Sy --needed --noconfirm bc coreutils cpio gettext initramfs kmod libelf ncurses pahole perl python3 tar xz && \
 wget -P /tmp https://cdn.kernel.org/pub/linux/kernel/v7.x/linux-7.2.6.tar.xz && \
 tar xvpf /tmp/linux-*.tar.xz -C /tmp --xattrs-include='*.*' --numeric-owner && \
 sudo rm -rf /tmp/linux-*.tar.xz && \
